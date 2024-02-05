cac8521425ae491a34426b1d7f111e8b1f3e4a9c
nitesh kuwarbi <niteshkuwarbi@gmail.com>
2024-02-04 15:21:04 -1200

    Add DSA_Python folder

---
+++ Dynamic Array/01creating_list.py
@@ -0,0 +1,16 @@
+import ctypes
+
+class apnaList:
+
+    def __init__(self):
+        self.size = 1
+        self.n = 0
+        # creating a c type array with size = self.size
+        self.A = self.__make__array(self.size)
+
+    def __make__array(self,capacity):
+        # this code is a ctype array(static, referential) with size capacity
+        return (capacity*ctypes.py_object)() 
+    
+L = apnaList()
+print(L) 
---
+++ Dynamic Array/04adding_print.py
@@ -0,0 +1,51 @@
+import ctypes
+
+class apnaList:
+
+    def __init__(self):
+        self.size = 1
+        self.n = 0
+        # creating a c type array with size = self.size
+        self.A = self.__make__array(self.size)
+    
+    def len(self):
+        return self.n
+    
+    def append(self, item):
+        if self.size == self.size:
+            # resize
+            self.__resize(self.size*2)
+
+        # append
+        self.A[self.n] = item
+        self.n = self.n + 1
+
+    def __resize(self, new_capacity):
+        # create a new array with new capacity
+        B = self.__make__array(new_capacity)
+        self.size = new_capacity
+        # copy the content of A to B
+        for i in range(self.n):
+            B[i] = self.A[i]
+        # reassign A
+        self.A = B
+
+    def __str__(self):
+        # [1,2,3]
+        result = ''
+        for i in range(self.n):
+            result = result + str(self.A[i]) + ','
+        
+        return '[' +  result[:-1] + ']'
+
+    def __make__array(self,capacity):
+        # this code is a ctype array(static, referential) with size capacity
+        return (capacity*ctypes.py_object)() 
+
+L = apnaList()
+L.append('hello')
+L.append(True)
+L.append(1008) 
+L.append(4.6) 
+
+print(L)
